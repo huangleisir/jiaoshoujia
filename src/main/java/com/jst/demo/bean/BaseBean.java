d10747ed0254e0e06f4cf80085057af0f8cfed90
huangleisir <huangleisir@qq.com>
2017-08-17 21:56:27 +0800

    反编译  去掉乱码

---
+++ src/main/java/com/jst/demo/bean/BaseBean.java
@@ -0,0 +1,16 @@
+package com.jst.demo.bean;
+
+public class BaseBean {
+
+	public Long getCurrentPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPageSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
