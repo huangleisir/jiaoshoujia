73eefaac09a9c6caac1f05377f843ba6febd38a1
huangleisir <huangleisir@qq.com>
2017-08-19 13:13:13 +0800

    add  各种数据类型的操作demo

--- src/main/java/com/jst/framework/redis/RedisUtils.java
+++ src/main/java/com/jst/framework/redis/RedisUtils.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
@@ -114,4 +115,66 @@ public class RedisUtils {
         }
         return result;
     }
+    
+    /**
+     * 写入缓存sadd
+     * 
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean sadd(final String key, Object value) {
+        boolean result = false;
+        try {
+            SetOperations<Serializable, Object> operations = redisTemplate.opsForSet();
+            operations.add(key, value);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+    /**
+		 //添加一个 key 
+        ValueOperations<String, Object> value = redisTemplate.opsForValue();
+        value.set("lp", "hello word");
+        //获取 这个 key 的值
+        System.out.println(value.get("lp"));
+        //添加 一个 hash集合
+        HashOperations<String, Object, Object>  hash = redisTemplate.opsForHash();
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("name", "lp");
+        map.put("age", "26");
+        hash.putAll("lpMap", map);
+        //获取 map
+        System.out.println(hash.entries("lpMap"));
+        //添加 一个 list 列表
+        ListOperations<String, Object> list = redisTemplate.opsForList();
+        list.rightPush("lpList", "lp");
+        list.rightPush("lpList", "26");
+        //输出 list
+        System.out.println(list.range("lpList", 0, 1));
+        //添加 一个 set 集合
+        SetOperations<String, Object> set = redisTemplate.opsForSet();
+        set.add("lpSet", "lp");
+        set.add("lpSet", "26");
+        set.add("lpSet", "178cm");
+        //输出 set 集合
+        System.out.println(set.members("lpSet"));
+        //添加有序的 set 集合
+        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
+        zset.add("lpZset", "lp", 0);
+        zset.add("lpZset", "26", 1);
+        zset.add("lpZset", "178cm", 2);
+        //输出有序 set 集合
+        System.out.println(zset.rangeByScore("lpZset", 0, 2));
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+    
+    
 }
